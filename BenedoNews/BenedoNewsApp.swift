a682912fd8fb607f5628f1b8d1958d2052e11deb
Fredisma0169 <97203352+Fredisma0169@users.noreply.github.com>
2023-03-06 22:30:25 +0100

    Initial Commit

---
+++ BenedoNews/BenedoNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenedoNewsApp.swift
+//  BenedoNews
+//
+//  Created by Fredisma OUEDRAOGO on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BenedoNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
